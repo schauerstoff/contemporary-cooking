3ccf0eaab5c9145830ed2821304684f3f68cd387
schauerstoff <ss.schmidt@gmx.de>
2025-05-16 12:58:18 +0200

    chore(ui): add tailwind

--- admin-ui/src/App.tsx
+++ admin-ui/src/App.tsx
@@ -3,7 +3,7 @@ import IngredientForm from "./components/IngredientForm";
 function App() {
   return (
     <div>
-      <h1>
+      <h1 className="underline">
         Admin UI
       </h1>
       <div>
